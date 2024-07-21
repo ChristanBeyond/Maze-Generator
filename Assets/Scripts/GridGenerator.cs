ed7a6ff9afd0cf4d6a96f9b08e93117c913fbeed
V Majarocon <56297035+Veerific@users.noreply.github.com>
2024-07-21 19:58:25 +0200

    Small fixes to the grid generator

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -9,7 +9,7 @@ using Random = UnityEngine.Random;
 public class GridGenerator : MonoBehaviour
 {
     //TODO: Check if I can store the grid information in a scriptable object
-    
+
     public int widthSize, heightSize;
     [SerializeField] private Cell mazeCell;
     public Cell[,] _cells;
@@ -29,18 +29,18 @@ public class GridGenerator : MonoBehaviour
     private IEnumerator GenerateGrid()
     {
         ResetMaze();
-        for (int y = 0; y < heightSize; y++)
+        for (int x = 0; x < widthSize; x++)
         {
-            for (int x = 0; x < widthSize; x++)
+            for (int y = 0; y < heightSize; y++)
             {
                 Cell newCell = Instantiate(mazeCell, gameObject.transform);
                 newCell.transform.position = new(x, y);
-                _cells[x,y] = newCell;
- 
+                _cells[x, y] = newCell;
+
                 yield return new WaitForSeconds(0.0001f);
             }
         }
-        
+
         var randomX = Random.Range(0, widthSize - 1);
         var randomY = Random.Range(0, heightSize - 1);
         _initialCell = _cells[randomX, randomY];
@@ -50,15 +50,16 @@ public class GridGenerator : MonoBehaviour
     //TODO: Write cheaper way to reset the grid
     private void ResetMaze()
     {
-        if (_cells[0,0] != null)
+        if (_cells[0, 0] != null)
         {
-            for (int y = 0; y < _cells.GetLength(0); y++)
+            for (int x = 0; x < _cells.GetLength(0); x++)
             {
-                for (int x = 0; x < _cells.GetLength(1); x++)
+                for (int y = 0; y < _cells.GetLength(1); y++)
                 {
                     Destroy(_cells[x, y].gameObject);
                 }
             }
+
             //Clears the array completely
             _cells = new Cell[widthSize, heightSize];
         }
